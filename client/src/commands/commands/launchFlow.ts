a03817f08b5dc2d0fd1de26ccf89f22ac41d7fec
Lucas Rouret <lucasro@bam.tech>
2023-12-17 10:50:22 +0100

    refacto(languageServer): move src client to client folder

---
+++ client/src/commands/commands/launchFlow.ts
@@ -0,0 +1,11 @@
+import * as vscode from 'vscode';
+import { ItemTree } from '../../treeView/FileSystemTreeBuilder';
+
+export function launchFlow(fileItem: ItemTree) {
+  const completePath = fileItem.command?.arguments?.[0];
+  if (completePath) {
+    const terminal = vscode.window.createTerminal();
+    terminal.sendText(`maestro test ${completePath}`);
+    terminal.show();
+  }
+}
